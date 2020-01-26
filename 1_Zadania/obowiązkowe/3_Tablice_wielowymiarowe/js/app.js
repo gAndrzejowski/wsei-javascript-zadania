9f4bd9c75535dd23b854c8f35018e462dbdf1b5f
Andrzejowski Grzegorz <grzegorz.andrzejowski@microsoft.wsei.edu.pl>
2020-01-26 14:16:51 +0100

    Zestaw 3

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,50 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+// Zadanie 0
+
+const checkArray = (dwuwymiarowa) => dwuwymiarowa.map(
+    podtablica => podtablica.every (
+        element => element % 2 === 0
+    )
+);
+
+console.log(checkArray([
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+]));
+
+// Zadanie 1
+
+console.log(task1Array[2][1]);
+console.log(task1Array[1].length);
+console.log(task1Array[3][1])
+
+// Zadanie 2
+
+for (let counter = 0; counter < task2Array.length; counter++) {
+    console.log(task2Array[counter]);
+}
+
+for (let counter = 0; counter < task2Array.length; counter++) {
+    console.log(task2Array[counter].length);
+}
+
+for (let counter1 = 0; counter1 < task2Array.length; counter1++) {
+    for (let counter2 = 0; counter2 < task2Array[counter1].length; counter2++) {
+        console.log(task2Array[counter1][counter2]);
+    }
+}
+
+// Zadanie 3
+
+const print2DArray = (tablica2D) => {
+    const splaszczone = tablica2D.reduce((acc, rzad) => {
+        acc.concat(rzad);
+    });
+    splaszczone.forEach(element => console.log(element));
+};
